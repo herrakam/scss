9a125bb4bf8c7234e369d9bf2354bad7df97a3da
SeungHyun <fm10033@gmail.com>
2021-08-11 16:25:28 +0900

    dialog 구현

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.scss";
 // import Checkbox from "./components/Checkbox";
 import styled, { ThemeProvider } from "styled-components";
 import StyledButtons from "./components/StyledButtons";
-
+import Dialog from "./components/Dialog";
 const Circle = styled.div`
   width: 5rem;
   height: 5rem;
@@ -32,61 +32,42 @@ const palette = {
 
 function App() {
   const [check, setCheck] = useState(false);
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
 
   const onchange = (e) => {
     setCheck(e.target.checked);
   };
+
+  const onConfirm = () => {
+    setDialog(false);
+    console.log("확인");
+  };
+  const onCancel = () => {
+    setDialog(false);
+    console.log("취소");
+  };
   return (
     <ThemeProvider theme={{ palette }}>
-      <div className="App">
-        {/* <Circle color="skyblue" huge></Circle>
-        <Circle color="black"></Circle> */}
-        <Buttons>
-          <StyledButtons size="large">Button</StyledButtons>
-          <StyledButtons>Button</StyledButtons>
-          <StyledButtons size="small">Button</StyledButtons>
-        </Buttons>
-        <Buttons>
-          <StyledButtons color="gray" size="large">
-            Button
+      <>
+        <div className="App">
+          <StyledButtons color="pink" size="large" onClick={onClick}>
+            삭제
           </StyledButtons>
-          <StyledButtons color="gray">Button</StyledButtons>
-          <StyledButtons color="gray" size="small">
-            Button
-          </StyledButtons>
-        </Buttons>
-        <Buttons>
-          <StyledButtons color="pink" size="large">
-            Button
-          </StyledButtons>
-          <StyledButtons color="pink">Button</StyledButtons>
-          <StyledButtons color="pink" size="small">
-            Button
-          </StyledButtons>
-        </Buttons>
-        <Buttons>
-          <StyledButtons size="large" outline>
-            Button
-          </StyledButtons>
-          <StyledButtons color="gray" outline>
-            Button
-          </StyledButtons>
-          <StyledButtons color="pink" size="small" outline>
-            Button
-          </StyledButtons>
-        </Buttons>
-        <Buttons>
-          <StyledButtons size="large" fullWidth>
-            Button
-          </StyledButtons>
-          <StyledButtons color="gray" size="large" fullWidth>
-            Button
-          </StyledButtons>
-          <StyledButtons color="pink" size="large" fullWidth>
-            Button
-          </StyledButtons>
-        </Buttons>
-      </div>
+        </div>
+        <Dialog
+          title="삭제 ㄱㄱ?"
+          confirmText="삭제"
+          cancelText="취소"
+          visible={dialog}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        >
+          데이터 ㄹㅇ 삭제 ㄱㄱ?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
